a97b2887bbdcad6dfbcda1860a3cfc2584aea30b
Saadh S <atextbooksituation@gmail.com>
2024-03-05 14:16:13 -0500

    Version done for normal server
    Removing serverless from this

--- express/server.js
+++ express/server.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require( 'express' );
 const path = require('path');
-const serverless = require( 'serverless-http' );
+// const serverless = require( 'serverless-http' );
 const app = express();
 const bodyParser = require( 'body-parser' );
 
@@ -163,4 +163,4 @@ app.use( '/.netlify/functions/server', router );  // path must route to lambda
 app.use( '/', ( req, res ) => res.sendFile( path.join( __dirname, '../index.html' ) ) );
 
 module.exports = app;
-module.exports.handler = serverless( app );
+// module.exports.handler = serverless( app );
---
+++ server.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const app = require('./express/server');
+const port = 3000;
+app.listen(port, () => console.log(`Local app listening on port ${ port }!`));
